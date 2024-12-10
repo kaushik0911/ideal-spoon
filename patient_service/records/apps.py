9eeaa84001981759f989af1fdea27bfeb9a5d552
kaushik911 <kaushikshamantha@gmail.com>
2024-12-10 12:35:58 +0400

    * Schema path added * Superbase DB setup

--- patient_service/records/apps.py
+++ patient_service/records/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
-
+from django.db import connection
 
 class RecordsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'records'
+
+    def ready(self):
+        # This will run when the app is ready
+        with connection.cursor() as cursor:
+            cursor.execute('SET search_path TO patient_service')
