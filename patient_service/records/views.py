260ea4abb08b4e3a5284244337d874423b9a66b6
kaushik911 <kaushikshamantha@gmail.com>
2024-12-18 19:55:17 +0400

    * patient service updates

--- patient_service/records/views.py
+++ patient_service/records/views.py
@@ -8,17 +8,17 @@ from .serializers import (
 )
 
 class PatientViewSet(viewsets.ModelViewSet):
-    queryset = Patient.objects.all()
+    queryset = Patient.objects.all()[:10]
     serializer_class = PatientSerializer
 
 class MedicalHistoryViewSet(viewsets.ModelViewSet):
-    queryset = MedicalHistory.objects.all()
+    queryset = MedicalHistory.objects.all()[:10]
     serializer_class = MedicalHistorySerializer
 
 class PrescriptionViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Prescription.objects.all()
+    queryset = Prescription.objects.all()[:10]
     serializer_class = PrescriptionSerializer
 
 class LabResultViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = LabResult.objects.all()
+    queryset = LabResult.objects.all()[:10]
     serializer_class = LabResultSerializer
